12652681b97ed4e721f0e3251eb353b5be2673e2
SEZER <stanci@icsltd.com.tr>
2020-11-29 14:06:53 +0300

    First

---
+++ AuthSystem/Areas/Extensions/AppClaimsPrincipalFactory.cs
@@ -0,0 +1,33 @@
+﻿using AuthSystem.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace AuthSystem.Areas.Extensions
+{
+    public class AppClaimsPrincipalFactory : UserClaimsPrincipalFactory<AuthSystemUser, IdentityRole>
+    {
+        public AppClaimsPrincipalFactory(
+           UserManager<AuthSystemUser> userManager
+           , RoleManager<IdentityRole> roleManager
+           , IOptions<IdentityOptions> optionsAccessor)
+       : base(userManager, roleManager, optionsAccessor)
+        { }
+
+        public async override Task<ClaimsPrincipal> CreateAsync(AuthSystemUser user)
+        {
+            var principal = await base.CreateAsync(user);
+            if (!string.IsNullOrWhiteSpace(user.FirstName))
+            {
+                ((ClaimsIdentity)principal.Identity).AddClaims(new[] { new Claim(ClaimTypes.GivenName, user.FirstName) });
+            }
+
+            if (!string.IsNullOrWhiteSpace(user.LastName))
+            {
+                ((ClaimsIdentity)principal.Identity).AddClaims(new[] { new Claim(ClaimTypes.Surname, user.LastName) });
+            }
+            return principal;
+        }
+    }
+}
---
+++ AuthSystem/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,32 @@
+﻿using AuthSystem.Areas.Identity.Data;
+using AuthSystem.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(AuthSystem.Areas.Identity.IdentityHostingStartup))]
+namespace AuthSystem.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) =>
+            {
+                services.AddDbContext<AuthSystemContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("AuthSystemContextConnection")));
+
+                services.AddDefaultIdentity<AuthSystemUser>(options =>
+                {
+                    options.SignIn.RequireConfirmedAccount = false;
+                    options.Password.RequireLowercase = false;
+                    options.Password.RequireUppercase = false;
+                    options.Password.RequireNonAlphanumeric = false;
+                })
+                    .AddEntityFrameworkStores<AuthSystemContext>();
+            });
+        }
+    }
+}
